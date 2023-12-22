6af0e809ae6480166bf0f268e2402922b3938afc
Sanay Nesargi <sanaynesargi@gmail.com>
2023-12-22 10:13:51 -1000

    Added New URI

--- pool-scoring-ui/utils/utils.ts
+++ pool-scoring-ui/utils/utils.ts
@@ -1,3 +1,3 @@
 const prod = process.env.NODE_ENV === "production";
 
-export const apiUrl = prod ? "34.216.190.181/api" : "localhost:8000/api";
+export const apiUrl = prod ? "35.88.136.43/api" : "localhost:8000/api";
