3fe4e65ad73445324e1db0ff2bc87148d264ff10
Sanay Nesargi <sanaynesargi@gmail.com>
2024-01-01 23:24:48 -0600

    chanes

--- pool-scoring-ui/src/app/page.tsx
+++ pool-scoring-ui/src/app/page.tsx
@@ -34,7 +34,7 @@ const HomePage: React.FC = () => {
     >
       <Container centerContent>
         <Heading mb={6} size="2xl" color="teal.500">
-          S-Pool v.1.3.0
+          S-Pool v.1.3.1
         </Heading>
 
         <HStack spacing={8}>
