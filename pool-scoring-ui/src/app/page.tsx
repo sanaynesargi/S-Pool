2c7d85acd428be3404e87344331704d53b250ee9
Sanay Nesargi <sanaynesargi@gmail.com>
2024-06-14 01:14:14 +0200

    T

--- pool-scoring-ui/src/app/page.tsx
+++ pool-scoring-ui/src/app/page.tsx
@@ -34,7 +34,7 @@ const HomePage: React.FC = () => {
     >
       <Container centerContent>
         <Heading mb={6} size="2xl" color="teal.500">
-          S-Pool v.1.8.2
+          S-Pool v.2.0.0
         </Heading>
 
         <HStack spacing={8}>
--- pool-scoring-ui/utils/utils.ts
+++ pool-scoring-ui/utils/utils.ts
@@ -1,3 +1,3 @@
 const prod = process.env.NODE_ENV === "production";
 
-export const apiUrl = prod ? "3.95.92.109/api" : "localhost:8000/api";
+export const apiUrl = prod ? "3.95.92.109/pool/api" : "localhost:8000/api";
