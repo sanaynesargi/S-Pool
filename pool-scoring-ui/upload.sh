a632128ea290de4323ef0ec8b89a5d4656046fad
Sanay Nesargi <sanaynesargi@gmail.com>
2024-06-14 00:53:11 +0200

    upload w/ root

--- pool-scoring-ui/upload.sh
+++ pool-scoring-ui/upload.sh
@@ -1,2 +1,2 @@
 yarn build
-rsync -avz --info=progress2 -e  "ssh -i ~/.ssh/fm-server.pem" .next/ ubuntu@ec2-18-208-137-72.compute-1.amazonaws.com:/home/ubuntu/S-Pool/pool-scoring-ui/.next
+rsync -avz --info=progress2 -e  "ssh -i ~/.ssh/fm-server.pem" .next/ ubuntu@ec2-18-208-137-72.compute-1.amazonaws.com:/home/ubuntu/S-Pool/pool-scoring-ui/.next  --rsync-path="sudo rsync"
