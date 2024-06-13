18d00a0ac146f7df37e7b9d70d10dfbe270b32be
Sanay Nesargi <sanaynesargi@gmail.com>
2024-06-13 23:40:36 +0200

    A

--- pool-scoring-ui/upload.sh
+++ pool-scoring-ui/upload.sh
@@ -1,2 +1,2 @@
-yarn build
+# yarn build
 rsync -avz --info=progress2 -e  "ssh -i ~/.ssh/fm-server.pem" node_modules/ ubuntu@ec2-18-233-157-127.compute-1.amazonaws.com:/home/ubuntu/S-Pool/pool-scoring-ui/node_modules
