4fd43987cfcc2341bbaa4b5678cf76e0630020eb
Sanay Nesargi <sanaynesargi@gmail.com>
2024-06-14 01:04:05 +0200

    T

--- pool-scoring-ui/utils/utils.ts
+++ pool-scoring-ui/utils/utils.ts
@@ -1,3 +1,3 @@
 const prod = process.env.NODE_ENV === "production";
 
-export const apiUrl = prod ? "44.226.227.51/api" : "localhost:8000/api";
+export const apiUrl = prod ? "3.95.92.109/api" : "localhost:8000/api";
